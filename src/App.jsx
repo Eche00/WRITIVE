d42681f34aeb9a5df0046ee0b93fe4d9c4ec76f5
Eche00 <echeeze956@gmail.com>
2025-07-06 06:11:57 +0100

    App routes edit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Route, Routes } from "react-router";
+import Container from "./component/Container";
+import Signup from "./authentication/Signup";
+import Signin from "./authentication/Signin";
+import ForgotPassword from "./authentication/ForgotPassword";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import Brand from "./pages/Brand";
+import ProductionWorkflow from "./pages/ProductionWorkflow";
+import Booking from "./pages/Booking";
+import Inventory from "./pages/Inventory";
+import Settings from "./pages/Settings";
+
+function App() {
+  return (
+    <div className=" text-white  overflow-hidden bg-white  dark:bg-[#1F1F1F]">
+      <Routes>
+        <Route element={<Container />}>
+          <Route path="/home" element={<Dashboard />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/brand" element={<Brand />} />
+          <Route path="/workflow" element={<ProductionWorkflow />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/settings" element={<Settings />} />
+
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/" element={<Signup />} />
+        </Route>
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
