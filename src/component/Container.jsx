b96edb463823a966dd3f2ee09df8bad94c859cd2
Eche00 <echeeze956@gmail.com>
2025-07-08 05:21:44 +0100

    imports

--- src/component/Container.jsx
+++ src/component/Container.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Outlet, useLocation, useNavigate } from "react-router";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router";
 import Header from "./Header";
 
 function Container() {
