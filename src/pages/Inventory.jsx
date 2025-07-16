f425fe1375cefe79236f28edc9ce573857bf2563
Eche00 <echeeze956@gmail.com>
2025-07-16 15:17:58 +0100

    inventory page animations and loader effect

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Search } from "@mui/icons-material";
+import UserLoader from "../component/UserLoader";
+import { motion } from "framer-motion";
 
 const BASE_URL = "https://65e435ef7c7e.ngrok-free.app";
 
@@ -30,60 +32,75 @@ const Inventory = () => {
 
   useEffect(() => {
     fetchInventory();
-  }, [search]);
+  }, []);
 
   return (
     <div className="p-4 md:w-[80%] w-fit overflow-scroll mx-auto text-black flex flex-col h-fit ">
-      <div className="flex items-center justify-between mb-6">
-        <h2 className="text-2xl font-bold text-[#412666]">Inventar</h2>
-        <div className="flex items-center mb-4 border border-[#412666] rounded-lg px-4 py-2 w-1/3 gap-2">
-          <Search className="text-[#412666]" />
-          <input
-            type="text"
-            placeholder="Suche nach  Kategorie.."
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            className="w-full border-none focus:outline-none"
-          />
-        </div>
-      </div>
+      <h2 className="text-2xl font-bold text-[#412666] mb-5">Inventar</h2>
+      {loading ? (
+        <section>
+          <UserLoader />
+        </section>
+      ) : (
+        <motion.div
+          initial={{ scale: 0.8, opacity: 0 }}
+          whileInView={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1.5, delay: 0.3 }}
+          className="bg-white p-4 rounded-xl shadow border border-gray-100 w-fit xl:w-full max-h-[700px] overflow-scroll">
+          <div className="flex items-center justify-between mb-6">
+            Inventar
+            <div className="flex items-center mb-4 border border-[#412666] rounded-lg px-4 py-2 w-1/3 gap-2">
+              <Search className="text-[#412666]" />
+              <input
+                type="text"
+                placeholder="Suche nach  Kategorie.."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="w-full border-none focus:outline-none"
+              />
+            </div>
+          </div>
 
-      <table className="w-full text-sm text-left">
-        <thead className="text-[#412666] border-b border-gray-200">
-          <tr>
-            <th className="py-2 px-3">Bezeichnung</th>
-            <th className="py-2 px-3">Kategorie</th>
-            <th className="py-2 px-3">Gesamtstückzahl</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.length === 0 && !loading ? (
-            <tr>
-              <td colSpan="3" className="text-center p-4 text-gray-500">
-                Keine Daten gefunden.
-              </td>
-            </tr>
-          ) : (
-            items.flatMap((item, catIdx) =>
-              item.Bezeichnungen.filter((bezeichnung) => {
-                const query = search.toLowerCase();
-                return (
-                  bezeichnung.Wert.toLowerCase().includes(query) ||
-                  item.Kategorie.toLowerCase().includes(query)
-                );
-              }).map((bezeichnung, idx) => (
-                <tr
-                  key={`${catIdx}-${idx}`}
-                  className="border-b border-gray-200 hover:bg-gray-50">
-                  <td className="py-2 px-3">{bezeichnung.Wert}</td>
-                  <td className="py-2 px-3">{item.Kategorie}</td>
-                  <td className="py-2 px-3">{bezeichnung.GesamtStueckzahl}</td>
+          <table className="w-full text-sm text-left">
+            <thead className="text-[#412666] border-b border-gray-200">
+              <tr>
+                <th className="py-2 px-3">Bezeichnung</th>
+                <th className="py-2 px-3">Kategorie</th>
+                <th className="py-2 px-3">Gesamtstückzahl</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.length === 0 && !loading ? (
+                <tr>
+                  <td colSpan="3" className="text-center p-4 text-gray-500">
+                    Keine Daten gefunden.
+                  </td>
                 </tr>
-              ))
-            )
-          )}
-        </tbody>
-      </table>
+              ) : (
+                items.flatMap((item, catIdx) =>
+                  item.Bezeichnungen.filter((bezeichnung) => {
+                    const query = search.toLowerCase();
+                    return (
+                      bezeichnung.Wert.toLowerCase().includes(query) ||
+                      item.Kategorie.toLowerCase().includes(query)
+                    );
+                  }).map((bezeichnung, idx) => (
+                    <tr
+                      key={`${catIdx}-${idx}`}
+                      className="border-b border-gray-200 hover:bg-gray-50">
+                      <td className="py-2 px-3">{bezeichnung.Wert}</td>
+                      <td className="py-2 px-3">{item.Kategorie}</td>
+                      <td className="py-2 px-3">
+                        {bezeichnung.GesamtStueckzahl}
+                      </td>
+                    </tr>
+                  ))
+                )
+              )}
+            </tbody>
+          </table>
+        </motion.div>
+      )}
     </div>
   );
 };
