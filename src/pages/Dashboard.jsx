0a144dcfd09f016334aa27c96c30565958db3785
Eche00 <echeeze956@gmail.com>
2025-07-16 12:57:13 +0100

    Uodating dashboard to look better

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,11 @@
 import {
+  Air,
+  Article,
   BrandingWatermark,
+  Category,
+  Info,
+  Inventory,
+  LibraryBooksOutlined,
   LightMode,
   People,
   Search,
@@ -15,6 +21,12 @@ const Dashboard = () => {
   const [currentUser, setCurrentUser] = useState(null);
   const [customers, setCustomers] = useState([]);
   const [brands, setBrands] = useState([]);
+  const [campaigns, setCampaigns] = useState([]);
+  const [articles, setArticles] = useState([]);
+  const [productions, setProductions] = useState([]);
+  const [bookings, setBookings] = useState([]);
+  const [items, setItems] = useState([]);
+  const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState("");
   const [filtered, setFiltered] = useState([]);
@@ -101,11 +113,134 @@ const Dashboard = () => {
       setLoading(false);
     }
   };
+  // FETCHING CAMPAIGNS
+  const fetchCampaigns = async () => {
+    try {
+      setLoading(true);
+      const token = localStorage.getItem("token");
+      const res = await fetch(`${BASE_URL}/campaigns/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "ngrok-skip-browser-warning": "true",
+        },
+      });
+      const data = await res.json();
+      setCampaigns(data);
+    } catch (error) {
+      console.error("Failed to fetch campaigns:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  // FETCHING ARTICLES
+  const fetchArticles = async () => {
+    setLoading(true);
+    try {
+      const token = localStorage.getItem("token");
+      const res = await fetch(`${BASE_URL}/products/`, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+          "ngrok-skip-browser-warning": "true",
+        },
+      });
+      const data = await res.json();
 
+      // ✅ Correctly extract the articles array
+      setArticles(data.articles);
+      console.log("Fetched articles:", data.articles || []);
+    } catch (err) {
+      console.error("Error fetching articles:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+  //  FETCHING PRODUCTIONS
+  const fetchProductions = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const res = await fetch(`${BASE_URL}/production/?search=${search}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          "ngrok-skip-browser-warning": "true",
+        },
+      });
+      const data = await res.json();
+      setProductions(data?.productions || []);
+    } catch (err) {
+      console.error("Fetch production failed:", err);
+    }
+  };
+  // FETCHING BOOKINGS
+  const fetchBookings = async () => {
+    setLoading(true);
+    try {
+      const token = localStorage.getItem("token");
+      const res = await fetch(`${BASE_URL}/buchungshistorie/`, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+          "ngrok-skip-browser-warning": "true",
+        },
+      });
+      const data = await res.json();
+      setBookings(data.bookings);
+    } catch (err) {
+      console.error("Error fetching bookings:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+  // FETCHING INVENTORY
+  const fetchInventory = async () => {
+    setLoading(true);
+    try {
+      const token = localStorage.getItem("token");
+      const res = await fetch(`${BASE_URL}/inventory/overview`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          "ngrok-skip-browser-warning": "true",
+        },
+      });
+      const data = await res.json();
+      setItems(data.inventory || data); // Adjust if your API wraps results
+    } catch (err) {
+      console.error("Fehler beim Abrufen des Inventars:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+  // FETCHING CATEGORIES
+  const fetchCategories = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const res = await fetch(`${BASE_URL}/categories/?search=${search}`, {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+          "ngrok-skip-browser-warning": "true",
+        },
+      });
+      const data = await res.json();
+      setCategories(data.categories || []);
+    } catch (err) {
+      console.error("Fetch failed:", err);
+    }
+  };
   useEffect(() => {
     fetchCurrentUser();
     fetchCustomers();
     fetchBrands();
+    fetchCampaigns();
+    fetchArticles();
+    fetchProductions();
+    fetchBookings();
+    fetchInventory();
+    fetchCategories();
   }, []);
 
   // handle if there is a user logged in
@@ -158,44 +293,19 @@ const Dashboard = () => {
         </div>
         {/* Info */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-          {/* Total Customers */}
-
+          {/* Total Users */}
           <div className="p-5 bg-white rounded-xl border border-gray-200 shadow-sm flex justify-between items-start hover:shadow-2xl hover:scale-[105%] transition-all duration-300 cursor-pointer">
             <div>
-              <p className="text-sm text-gray-500 mb-1">Gesamtkunden</p>
+              <p className="text-sm text-gray-500 mb-1">Nutzer</p>
               <h3 className="text-2xl font-bold text-[#412666]">{totalUser}</h3>
               <p className="text-sm text-green-600 font-medium mt-1">
-                {totalUser} Nutzer
+                +{totalUser} Nutzer
               </p>
               <p className="text-xs text-gray-400 mt-1">Gesamte Nutzer</p>
             </div>
-            <div className="bg-[#f5f3f7] p-2 rounded-full">
+            <div className="bg-[#f5f3f7] w-[40px] h-[40px] flex items-center justify-center rounded-full">
               <span className="text-[#412666] text-xl">
                 <People />
-              </span>
-            </div>
-          </div>
-
-          {/* Active Customers */}
-          <div className="p-5 bg-white rounded-xl border border-gray-200 shadow-sm flex justify-between items-start hover:shadow-2xl hover:scale-[105%] transition-all duration-300 cursor-pointer">
-            <div>
-              <p className="text-sm text-gray-500 mb-1">Aktiv</p>
-              <h3 className="text-2xl font-bold text-[#412666]">
-                {activeUserCount}
-              </h3>
-              <p
-                className={`text-sm font-medium mt-1 ${
-                  activeUserCount > 0 ? "text-green-600" : "text-red-600"
-                }`}>
-                {activeUserCount} aktive Nutzer
-              </p>
-              <p className="text-xs text-gray-400 mt-1">
-                Gesamte aktive Nutzer
-              </p>
-            </div>
-            <div className="bg-[#f5f3f7] p-2 rounded-full">
-              <span className="text-[#412666] text-xl">
-                <LightMode />
               </span>
             </div>
           </div>
@@ -210,13 +320,127 @@ const Dashboard = () => {
               <p className="text-sm text-green-600 font-medium mt-1">
                 +{brands.length} Marken
               </p>
-              <p className="text-xs text-gray-400 mt-1">
-                Gesamte aktive Marken
-              </p>
+              <p className="text-xs text-gray-400 mt-1">Gesamte Marken</p>
             </div>
-            <div className="bg-[#f5f3f7] p-2 rounded-full">
+            <div className="bg-[#f5f3f7] w-[40px] h-[40px] flex items-center justify-center rounded-full">
               <span className="text-[#412666] text-xl">
                 <BrandingWatermark />
+              </span>
+            </div>
+          </div>
+          {/* campaign  */}
+          <div className="p-5 bg-white rounded-xl border border-gray-200 shadow-sm flex justify-between items-start hover:shadow-2xl hover:scale-[105%] transition-all duration-300 cursor-pointer">
+            <div>
+              <p className="text-sm text-gray-500 mb-1">Kampagne</p>
+              <h3 className="text-2xl font-bold text-[#412666]">
+                {campaigns.length}
+              </h3>
+              <p className="text-sm text-green-600 font-medium mt-1">
+                +{campaigns.length} Kampagne
+              </p>
+              <p className="text-xs text-gray-400 mt-1">Gesamte Kampagne</p>
+            </div>
+            <div className="bg-[#f5f3f7] w-[40px] h-[40px] flex items-center justify-center rounded-full">
+              <span className="text-[#412666] text-xl">
+                <Air />
+              </span>
+            </div>
+          </div>
+          {/* articles  */}
+          <div className="p-5 bg-white rounded-xl border border-gray-200 shadow-sm flex justify-between items-start hover:shadow-2xl hover:scale-[105%] transition-all duration-300 cursor-pointer">
+            <div>
+              <p className="text-sm text-gray-500 mb-1">Artikelverwaltung</p>
+              <h3 className="text-2xl font-bold text-[#412666]">
+                {articles.length}
+              </h3>
+              <p className="text-sm text-green-600 font-medium mt-1">
+                +{articles.length} Artikelverwaltung
+              </p>
+              <p className="text-xs text-gray-400 mt-1">
+                Gesamte Artikelverwaltung
+              </p>
+            </div>
+            <div className="bg-[#f5f3f7] w-[40px] h-[40px] flex items-center justify-center rounded-full">
+              <span className="text-[#412666] text-xl">
+                <Article />
+              </span>
+            </div>
+          </div>
+          {/* PRODUCTION  */}
+          <div className="p-5 bg-white rounded-xl border border-gray-200 shadow-sm flex justify-between items-start hover:shadow-2xl hover:scale-[105%] transition-all duration-300 cursor-pointer">
+            <div>
+              <p className="text-sm text-gray-500 mb-1">Produktionsablauf</p>
+              <h3 className="text-2xl font-bold text-[#412666]">
+                {productions.length}
+              </h3>
+              <p className="text-sm text-green-600 font-medium mt-1">
+                +{productions.length} Produktionsablauf
+              </p>
+              <p className="text-xs text-gray-400 mt-1">
+                Gesamte Produktionsablauf
+              </p>
+            </div>
+            <div className="bg-[#f5f3f7] w-[40px] h-[40px] flex items-center justify-center rounded-full">
+              <span className="text-[#412666] text-xl">
+                <Info />
+              </span>
+            </div>
+          </div>
+          {/* BOOKING  */}
+          <div className="p-5 bg-white rounded-xl border border-gray-200 shadow-sm flex justify-between items-start hover:shadow-2xl hover:scale-[105%] transition-all duration-300 cursor-pointer">
+            <div>
+              <p className="text-sm text-gray-500 mb-1">Buchungshistorie</p>
+              <h3 className="text-2xl font-bold text-[#412666]">
+                {bookings.length}
+              </h3>
+              <p className="text-sm text-green-600 font-medium mt-1">
+                +{bookings.length} Buchungshistorie
+              </p>
+              <p className="text-xs text-gray-400 mt-1">
+                Gesamte Buchungshistorie
+              </p>
+            </div>
+            <div className="bg-[#f5f3f7] w-[40px] h-[40px] flex items-center justify-center rounded-full">
+              <span className="text-[#412666] text-xl">
+                <LibraryBooksOutlined />
+              </span>
+            </div>
+          </div>
+          {/* Inventory  */}
+          <div className="p-5 bg-white rounded-xl border border-gray-200 shadow-sm flex justify-between items-start hover:shadow-2xl hover:scale-[105%] transition-all duration-300 cursor-pointer">
+            <div>
+              <p className="text-sm text-gray-500 mb-1">Inventarsystem</p>
+              <h3 className="text-2xl font-bold text-[#412666]">
+                {items.length}
+              </h3>
+              <p className="text-sm text-green-600 font-medium mt-1">
+                +{items.length} Inventarsystem
+              </p>
+              <p className="text-xs text-gray-400 mt-1">
+                Gesamte Inventarsystem
+              </p>
+            </div>
+            <div className="bg-[#f5f3f7] w-[40px] h-[40px] flex items-center justify-center rounded-full">
+              <span className="text-[#412666] text-xl">
+                <Inventory />
+              </span>
+            </div>
+          </div>
+          {/* ACategories */}
+          <div className="p-5 bg-white rounded-xl border border-gray-200 shadow-sm flex justify-between items-start hover:shadow-2xl hover:scale-[105%] transition-all duration-300 cursor-pointer">
+            <div>
+              <p className="text-sm text-gray-500 mb-1">Kategorien</p>
+              <h3 className="text-2xl font-bold text-[#412666]">
+                {categories.length}
+              </h3>
+              <p className="text-sm text-green-600 font-medium mt-1">
+                +{categories.length} Kategorien
+              </p>
+              <p className="text-xs text-gray-400 mt-1">Gesamte Kategorien</p>
+            </div>
+            <div className="bg-[#f5f3f7] w-[40px] h-[40px] flex items-center justify-center rounded-full">
+              <span className="text-[#412666] text-xl">
+                <Category />
               </span>
             </div>
           </div>
@@ -228,7 +452,7 @@ const Dashboard = () => {
           <UserLoader />
         </section>
       ) : (
-        <div className="bg-white p-4 rounded-xl shadow border border-gray-100 hover:shadow-2xl hover:scale-[101%] transition-all duration-300 ">
+        <div className="bg-white p-4 rounded-xl shadow border border-gray-100 hover:shadow-2xl hover:scale-[101%] transition-all duration-300  md:flex hidden flex-col">
           <div className="flex justify-between items-center mb-4">
             <h2 className="text-xl font-semibold text-[#412666] ">Nutzer</h2>
             <div className="border border-[#412666] rounded-lg px-4  w-1/3 focus:outline-none flex items-center gap-[10px] overflow-hidden">
