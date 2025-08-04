ef6462ab208024c538a7bb48c9acc2ab3cf71fd6
Eche00 <echeeze956@gmail.com>
2025-08-04 14:15:42 +0100

    Format images to map through on format view

---
+++ src/assets/index.js
@@ -0,0 +1,25 @@
+import Format1 from "/format1.webp";
+import Format2 from "/format2.webp";
+import Format3 from "/format3.webp";
+import Format4 from "/format4.webp";
+import Format5 from "/format5.webp";
+import Format6 from "/format6.webp";
+import Format7 from "/format7.webp";
+import Format8 from "/format8.webp";
+import Format9 from "/format9.webp";
+import Format10 from "/format10.webp";
+import Format11 from "/format11.webp";
+
+export {
+  Format1,
+  Format2,
+  Format3,
+  Format4,
+  Format5,
+  Format6,
+  Format7,
+  Format8,
+  Format9,
+  Format10,
+  Format11,
+};
