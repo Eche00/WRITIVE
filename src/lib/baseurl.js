10390eb262446d3404cf64c5da01cd578b1c7290
Eche00 <echeeze956@gmail.com>
2025-08-20 14:27:19 +0100

    Base Url

--- src/lib/baseurl.js
+++ src/lib/baseurl.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://6d5d2017a48e.ngrok-free.app";
+// export const BASE_URL = "https://6d5d2017a48e.ngrok-free.app";
+export const BASE_URL = "https://backend.writive.de";
